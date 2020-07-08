3764b63024576b1aab06cc8536c65efbf3044986
Josh Brooks <defaultjosh@hotmail.com>
2020-07-08 13:13:30 -0400

    Built a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="two-toned-footer-color">
+                <p className="text-muted text-muted-footer text-center">
+                    &copy; Copyright 2020 Joshua Brooks
+                </p> 
+            </div>
+        </footer>
+    )
+}
