fbe52621d35ee3405fbf1aa02e1a67bd3b17be15
Josh Brooks <defaultjosh@hotmail.com>
2020-07-08 14:34:54 -0400

    Changed import and set up for navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
 import Footer from "./components/Footer";
+import MyNavbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
+        <MyNavbar />
         <Route exact path="/" component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} /> 
