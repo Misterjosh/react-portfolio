f9202c5706e053a60a548100db416488b7207c87
Josh Brooks <defaultjosh@hotmail.com>
2020-07-08 14:37:09 -0400

    Removed text-muted to show text color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import "./styles/footer.css";
 
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="two-toned-footer-color">
-                <p className="text-muted text-muted-footer text-center">
+                <p className="text-muted-footer text-center">
                     &copy; Copyright 2020 Joshua Brooks
                 </p> 
             </div>
         </footer>
     )
 }
+
+// removed text-muted as a class to make the text color visible
