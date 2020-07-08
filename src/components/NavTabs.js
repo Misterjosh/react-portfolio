b0069ec7085e2278023f30315696beb832551e76
Josh Brooks <defaultjosh@hotmail.com>
2020-07-08 13:15:13 -0400

    Set up routing

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link, useLocation } from "react-router-dom";
 
-export default function NavTabs() {
+function NavTabs() {
+    const location = useLocation();
+
     return (
-        <div>
-            
-        </div>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link 
+                to="/" 
+                className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    About
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link 
+                to="/portfolio" 
+                className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                    Portfolio
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link 
+                to="/contact" 
+                className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                    Contact
+                </Link>
+            </li>
+        </ul>
     )
 }
+
+export default NavTabs;
