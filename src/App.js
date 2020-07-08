60ca9654309f04d6bcaa5feab1f1a35300a37374
Josh Brooks <defaultjosh@hotmail.com>
2020-07-08 13:12:00 -0400

    Added imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    null
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} /> 
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
