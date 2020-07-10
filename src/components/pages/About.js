77916f90b9b334f4f6eab593c54d667ad27eece1
Josh Brooks <defaultjosh@hotmail.com>
2020-07-10 16:41:51 -0400

    Added cards and collapse button

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,12 +2,15 @@ import React from 'react';
 import "../styles/about.css";
 import seanconnery from "../images/shelf.jpg";
 import { UncontrolledCollapse, Button, CardBody, Card } from 'reactstrap';
+import github from "../images/github.png";
+import linkedIn from "../images/linkedin.png";
+
 
 export default function About() {
     return (
-        <div>
+        <div className="container cards">
             
-            <main className="container">
+            <main>
                 <section className="row">
                 <div className="col-md-8">
                     <article className="block">
@@ -16,11 +19,11 @@ export default function About() {
                             data-toggle="tooltip" 
                             data-placement="bottom" 
                             title="Filler Photo Until I have a more professional one available" 
-                            alt="" id="A Sean Connery Shelf Meme" 
+                            alt="A Sean Connery Shelf Meme" 
                             className="me-photo"/>
 
-                                <p>I am a Full Stack Web Developer. 
-                                I have recently completed Case Western Reserve's Coding Bootcamp. 
+                                <p className="non-header">Hello. I am Joshua Brooks. I am a Full Stack Web Developer. 
+                                I have recently completed Case Western Reserve University's Coding Bootcamp. 
                                 I'm transitioning to a new careerfield, and look forward to learning and growing with your company.
                                 </p>
 
@@ -42,6 +45,32 @@ export default function About() {
                     </div>
                 </section>
             </main>
+            
+<main className="container cards card-holder">
+    <section className="row card-holder">
+        <div className="col-sm-6 col-md-6 col-lg-5">
+            <div className="card card-holder" style={{ width: "18rem" }}>
+                <img src={github} className="card-img-top" alt="Github Logo" />
+                <div className="card-body">
+                    <h5 className="card-title">My Github Repo</h5>
+                    <p className="card-title">A sample of my repo usage.</p>
+                    <a href="https://github.com/Misterjosh" target="_blank" rel="noopener noreferrer" className="btn btn-primary">My Repository</a>
+                </div>
+            </div>
+        </div>
+        <div className="col-sm-6 col-md-6 col-lg-5">
+            <div className="card card-holder" style={{ width: "18rem" }}>
+                <img src={linkedIn} className="card-img-top" alt="Github Logo" />
+                <div className="card-body">
+                    <h5 className="card-title">My LinkedIn Profile</h5>
+                    <p className="card-title">My Employment History.</p>
+                    <a href="https://www.linkedin.com/in/joshua-brooks-239a331a1/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">To LinkedIn</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
         </div>
     )
 }
