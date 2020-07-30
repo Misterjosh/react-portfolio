c98c887ef01d1c8a02b26da1d596dfb41be327fe
Josh Brooks <defaultjosh@hotmail.com>
2020-07-29 22:02:27 -0400

    Commented React.Strict out. Removed library errors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// StrictMode was throwing errors against libraries I was using.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
