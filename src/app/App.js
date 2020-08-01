9ff8b4088cc60ad74790434d7d9b41888089e2b4
Josh Brooks <defaultjosh@hotmail.com>
2020-07-31 22:59:58 -0400

    Moved App.js and style sheet into seprate folder

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Portfolio from "./pages/portfolio/Portfolio";
-import Contact from "./pages/contact/Contact";
-import Timecapsule from "./pages/timecapsule/Timecapsule";
-import Footer from "./components/footer/Footer";
-import MyNavbar from "./components/navbar/Navbar";
-import "./styles/app.css";
+import Home from "../pages/home/Home";
+import Portfolio from "../pages/portfolio/Portfolio";
+import Contact from "../pages/contact/Contact";
+import Timecapsule from "../pages/timecapsule/Timecapsule";
+import Footer from "../components/footer/Footer";
+import MyNavbar from "../components/navbar/Navbar";
+import "./app.css";
 
 function App() {
   return (
