2fa9f8fa52c8a19c2ac07e1e1be7179cb98c0fef
Josh Brooks <defaultjosh@hotmail.com>
2020-08-02 14:27:44 -0400

    Removed Portfolio page to make it a component on home page

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
-import Portfolio from "../pages/portfolio/Portfolio";
+// import Portfolio from "../pages/portfolio/Portfolio";
 import Contact from "../pages/contact/Contact";
 import Timecapsule from "../pages/timecapsule/Timecapsule";
 import Footer from "../components/footer/Footer";
@@ -14,7 +14,7 @@ function App() {
       <div className="App">
         <MyNavbar />
         <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
+        {/* <Route exact path="/portfolio" component={Portfolio} /> */}
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/timecapsule" component={Timecapsule} /> 
         <Footer />
