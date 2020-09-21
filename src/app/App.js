a8ef4fa02a5933fc5e5451a85a7c6c1041703ded
Josh Brooks <defaultjosh@hotmail.com>
2020-09-21 14:03:22 -0400

    refactored code

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Contact from "../pages/contact/Contact";
-// import Timecapsule from "../pages/timecapsule/Timecapsule";
 import Footer from "../components/footer/Footer";
 import MyNavbar from "../components/navbar/Navbar";
 import "./app.css";
@@ -14,7 +13,6 @@ function App() {
         <MyNavbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/contact" component={Contact} />
-        {/* <Route exact path="/timecapsule" component={Timecapsule} />  */}
         <Footer />
       </div>
     </Router>
